c6f406d29a5727efcfcd5ea5705385c41bb1d8ab
beiyong01 <beiyong01@beiyong01.sx-hl.com>
2015-06-03 20:29:48 +0800

    11111

---
+++ src/com/jmdns/multicast/device/ProtoSocket.java
@@ -0,0 +1,34 @@
+package com.jmdns.multicast.device;
+
+import android.content.Context;
+
+/*
+ * File：ProtoSocket.java
+ *
+ * Copyright (C) 2015 JmdnsClient Project
+ * Date：2015年6月3日 下午7:22:08
+ * All Rights SXHL(New Space) Corporation Reserved.
+ * http://www.at-et.cn
+ *
+ */
+
+/**
+ * @description:
+ * 
+ * @author: HuJun
+ * @date: 2015年6月3日 下午7:22:08
+ */
+
+public class ProtoSocket implements Runnable {
+	
+	public ProtoSocket(Context context){
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
